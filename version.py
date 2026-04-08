5ccad88596037181732c767f56504f432db4a70c
Tom Dalby <thomasjackdalby@gmail.com>
2026-04-08 06:14:47 +0100

    hook update

--- version.py
+++ version.py
@@ -1,8 +1,8 @@
 # auto-generated
                 
 class Version:
-    hash = "58333203eee8787d6e100ee09aae6f0e19c7ed66"
-    version = "0.0.7"
+    hash = "ddd4fb9860170910c07fd70cfc0a553b749b3010"
+    version = "0.0.9"
 
 if __name__ == "__main__":
     print(Version.version)
